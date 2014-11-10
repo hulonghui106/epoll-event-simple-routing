a61b1836558c28b3594676699ace1913269a4c04
hulonghui106 <hulonghui106@qq.com>
2014-11-10 15:11:20 +0800

    Update client.c

--- client/client.c
+++ client/client.c
@@ -25,43 +25,38 @@ int main(int argc, char *argv[])
     
     
     //char buf[MAXLINE]="a0001\0 a0001\0 abcdefg\0";
-    char buf[MAXLINE]="0000000001000000000201276878898932994324234325435sdfdsf";
+    char buf[MAXLINE]="0000000001000000000101276878898932994324234325435sdfdsf";
     
     
     int sockfd;
     
-    /* ============sockfd============ */
+      /* ============sockfd============ */
     sockfd = Socket(AF_INET, SOCK_STREAM, 0);
-    
+
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = inet_addr(host);
     servaddr.sin_port = htons(port);
     /* ============connect============ */
     Connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-    
-    /* =========stress testing======== */
-    
-    if(write(sockfd,buf,strlen(buf))==-1){
-        printf("write error\n");
-    }
-    
-     printf("write %lu\n",strlen(buf));
-    
-    
+
+
     while(1)
     {
+       if(write(sockfd,buf,strlen(buf))==-1){
+         printf("write error\n");
+       }
+       sleep(1);
+      // printf("write %lu\n",strlen(buf));
+
         int length = read(sockfd, buf, MAXLINE);
-        
+
         if (length>0) {
-            break;
+            printf("return %s\n",buf+20);
         }
-       
+
     }
 
-    printf("return %s\n",buf+20);
-   
-
-    return 0;  
+    return 0;
 }
 
